4a9911ec5339733c7795b179b3556568b5a893da
dxenia <marascadanaxenia@gmail.com>
2023-07-24 15:34:03 +0200

    Fixed the connection between database and contact form.

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+console.log('Hello from script.js!');
+// navigation
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -12,3 +14,40 @@ document.querySelectorAll('.nav-link').forEach((n) =>
     navMenu.classList.remove('active');
   })
 );
+
+// contact form
+
+const form = document.querySelector('.contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+
+  fetch('http://localhost:3000/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Issue with network response.');
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      alert('Your message was submitted successfully!');
+    })
+    .catch((error) => {
+      console.error(error);
+      alert('Error while submitting message.');
+    });
+});
--- src/server.js
+++ src/server.js
@@ -1,11 +1,47 @@
 const express = require('express');
-const connection = require('./db-connection');
 const app = express();
 const PORT = 3000;
+const mysql = require('mysql2');
+const cors = require('cors');
+const { join } = require('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'thisisthePasswordforrootuser1995o7o9',
+  database: 'portfolio',
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('Successful connection to the database!');
+  }
+});
 
 app.use(express.static('public'));
+
+app.post('/', (req, res) => {
+  const { name, email, number, message, date } = req.body;
+
+  const query =
+    'INSERT INTO messages (contact_name, contact_email, contact_number, content, date) VALUES (?, ?, ?, ?, NOW())';
+  const values = [name, email, number, message, date];
+
+  connection.query(query, values, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error submitting message' });
+      return;
+    }
+    res.status(201).json({ message: 'New message submitted', results });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
 });
-
